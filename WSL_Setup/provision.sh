153f895655fd55a3f9a6203ac67a2e2a80568db9
matej-macak <matej.macak@gmail.com>
2020-02-22 23:52:22 +0000

    Corrected NODEJS version

--- WSL_Setup/provision.sh
+++ WSL_Setup/provision.sh
@@ -4,6 +4,7 @@
 
 export PYTHON_VERSION=3.7.2
 export PIP_VERSION=${PYTHON_VERSION:0:3}
+export NODEJS_VERSION=10
 # export PIP_VERSION=3.7
 
 # Update dependencies
@@ -31,7 +32,7 @@ cd ~
 sudo apt-get install gfortran libopenblas-dev liblapack-dev --assume-yes
 
 # Install NodeJs for Jupyter Lab
-sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo curl -sL https://deb.nodesource.com/setup_$NODEJS_VERSION.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
 # Install python packages
@@ -47,26 +48,6 @@ sudo apt-get update && sudo apt-get install yarn -y
 
 # Install holoviews and geoviews for python visualisation
 sudo jupyter labextension install @pyviz/jupyterlab_pyviz
-
-# # Install geoviews
-# sudo pip3.6 install cython
-# sudo pip3.6 install shapely
-# sudo pip3.6 install pyshp
-# sudo pip3.6 install pillow
-# sudo pip3.6 install xarray
-# sudo apt-get install proj-bin -y
-# sudo apt-get install libproj-dev -y
-# sudo apt-get install binutils gdal-bin -y
-
-# sudo wget http://download.osgeo.org/geos/geos-3.6.3.tar.bz2
-# sudo bzip2 -d geos-3.6.3.tar.bz2
-# sudo tar -xvf geos-3.6.3.tar
-# cd geos-3.6.3/
-# sudo ./configure
-# sudo make
-# sudo make install
-# cd ..
-
 
 # Install git
 sudo apt-get install -y git
